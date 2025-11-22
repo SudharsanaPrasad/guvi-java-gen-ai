a03f1ac25d80f486b83493be086e14383717d69e
Nikhilnair48 <nikhil.nair48@gmail.com>
2025-11-22 21:36:10 +0530

    chore: updated custom sorting w/ comments

--- lecture-24/live-coding/lecture-24/src/com/guvi/comparable/CustomSorting.java
+++ lecture-24/live-coding/lecture-24/src/com/guvi/comparable/CustomSorting.java
@@ -11,10 +11,16 @@ public class CustomSorting {
             new Student(2, "Thirumalini"),
         };
 
-        //Arrays.sort(students);
-        //System.out.println("Sorting by ID: " + Arrays.toString(students));
+        // uncomment to see how compareTo method in Student class performs the sort
+        // Arrays.sort algorithm will call compareTo in the Student class
+        // anytime it needs to compare two Students
+        // Arrays.sort(students);
+        // System.out.println("Sorting by ID: " + Arrays.toString(students));
 
-        Arrays.sort(students, new NameComparator());
-        System.out.println("Sorting by name: " + Arrays.toString(students));
+        // uncomment to see how NameComparator's compare method performs the sort
+        // Arrays.sort algorithm will call compare in NameComparator
+        // anytime it needs to compare two Students
+        // Arrays.sort(students, new NameComparator());
+        // System.out.println("Sorting by name: " + Arrays.toString(students));
     }
 }
